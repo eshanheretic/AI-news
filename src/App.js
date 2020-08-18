cf18452f51ba3ddf34c4a14e0060069556ce8ce5
Eshan tripathi <eshan1285@gmail.com>
2020-08-18 19:10:33 +0530

    init the repository

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import logo from "./logo.png";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
+import wordsToNumbers from "words-to-numbers";
+const alanKey = process.env.REACT_APP_API_KEY;
 function App() {
+  const [newsArticles, setNewsArtilces] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArtilces(articles);
+          setActiveArticle(-1);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsed =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          if (parsed > 20) {
+            alanBtn().playText("Please try a different number");
+          } else {
+            window.open(articles[parsed - 1].url, "_blank");
+          }
+        }
+      },
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} />
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 }
